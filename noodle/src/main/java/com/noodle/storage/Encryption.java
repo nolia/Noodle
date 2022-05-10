a32fb9e8d84e9163aa3a5d5002fcb6808a27689d
Anton Penkrat <virtan.anton@gmail.com>
2022-05-10 11:53:05 +0300

    Add key argument to encrypt/decrypt methods
    Now Encryption implementation may use key of data for
    encrypt/decrypt algorithm

--- noodle/src/main/java/com/noodle/storage/Encryption.java
+++ noodle/src/main/java/com/noodle/storage/Encryption.java
@@ -7,31 +7,33 @@ public interface Encryption {
 
   /**
    * Cipher the data
+   * @param key key of persisted data
    * @param data data to cypher
    * @return cyphered data
    * @throws Exception
    */
-  byte[] encrypt(byte[] data) throws Exception;
+  byte[] encrypt(byte[] key, byte[] data) throws Exception;
 
   /**
    * Decrypts the data
+   * @param key key of persisted data
    * @param data encrypted data
    * @return decrypted data
    * @throws Exception
    */
-  byte[] decrypt(byte[] data) throws Exception;
+  byte[] decrypt(byte[] key, byte[] data) throws Exception;
 
   /**
    * Does not encrypt data, just returns the input.
    */
   Encryption NO_ENCRYPTION = new Encryption() {
     @Override
-    public byte[] encrypt(byte[] data) throws Exception {
+    public byte[] encrypt(byte[] key, byte[] data) throws Exception {
       return data;
     }
 
     @Override
-    public byte[] decrypt(byte[] data) throws Exception {
+    public byte[] decrypt(byte[] key, byte[] data) throws Exception {
       return data;
     }
   };
